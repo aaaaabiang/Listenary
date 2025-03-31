01102700386c066eca20d38d90fcacad0306d640
Liyuan Sun <liyuans@kth.se>
2025-03-31 22:08:58 +0200

    api configue

---
+++ src/apiConfig.js
@@ -0,0 +1,4 @@
+export const PROXY_URL =
+  "https://northeurope.api.cognitive.microsoft.com/speechtotext/transcriptions:transcribe?api-version=2024-11-15";
+export const PROXY_KEY =
+  "AQnGvGegFW18i0ViKLeI9B3Ujh04CciXFqntFfKLNuP93h8BjWUxJQQJ99BCACi5YpzXJ3w3AAAYACOGEm8J";
